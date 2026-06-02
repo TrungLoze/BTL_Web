0ec412732fa6f52a9ff04837493685c64629dd71
Nguyen Hoang Trung <nguyenhoangtrung26092006@gmail.com>
2026-06-02 22:31:57 +0700

    feat: implement home page layout, site-wide header/footer, and database schema for course management

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home');
 });
 
 app.listen(port, () => {
